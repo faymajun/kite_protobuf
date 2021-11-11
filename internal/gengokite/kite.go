b1ea51e616eb73ba4f8ae75d079225e3f1478199
faymajun <faymajun@gmail.com>
2021-11-11 16:14:42 +0800

    kite optimize

--- internal/gengokite/kite.go
+++ internal/gengokite/kite.go
@@ -1,9 +1,7 @@
 package gengokite
 
 import (
-	"fmt"
 	"path"
-	"strconv"
 	"strings"
 
 	"google.golang.org/protobuf/compiler/protogen"
@@ -11,11 +9,9 @@ import (
 )
 
 const (
-	// 接口
-	kitePackage = protogen.GoImportPath("git.dhgames.cn/svr_comm/kite/pkg/invoker")
-	kite        = protogen.GoImportPath("git.dhgames.cn/svr_comm/kite")
-	kiteClient  = protogen.GoImportPath("git.dhgames.cn/svr_comm/kite/cmd/client")
-	kiteAsync   = "Async"
+	PB     = protogen.GoImportPath("git.dhgames.cn/svr_comm/kite/pb")
+	kite   = protogen.GoImportPath("git.dhgames.cn/svr_comm/kite")
+	errors = protogen.GoImportPath("errors")
 )
 
 // GenerateFile generates a _kite.pb.go file containing gRPC service definitions.
@@ -39,64 +35,23 @@ func GenerateFileContent(gen *protogen.Plugin, file *protogen.File, g *protogen.
 		return
 	}
 
-	// TODO: Remove this. We don't need to include these references any more.
-	g.P("// Reference imports to suppress errors if they are not otherwise used.")
-	g.P("var _ ", kitePackage.Ident("IClient"))
-	g.P()
-
-	g.P("// This is a compile-time assertion to ensure that this generated file")
-	g.P("// is compatible with the kite package it is being compiled against.")
-	g.P("const _ = ", kitePackage.Ident("SupportPackageIsVersion6"))
-	g.P()
 	for _, service := range file.Services {
 		genService(gen, file, g, service)
 	}
 }
 
 func genService(gen *protogen.Plugin, file *protogen.File, g *protogen.GeneratedFile, service *protogen.Service) {
-	clientName := service.GoName + "Client"
+	clientName := export(service.GoName) + "Client"
 
 	g.P("// ", clientName, " is the client API for ", service.GoName, " service.")
 	g.P("//")
 	g.P("// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/kite#ClientConn.NewStream.")
 
-	// Client interface.
-	if service.Desc.Options().(*descriptorpb.ServiceOptions).GetDeprecated() {
-		g.P("//")
-		g.P(deprecationComment)
-	}
-	g.Annotate(clientName, service.Location)
-	g.P("type ", clientName, " interface {")
-	for _, method := range service.Methods {
-		g.Annotate(clientName+"."+method.GoName, method.Location)
-		if method.Desc.Options().(*descriptorpb.MethodOptions).GetDeprecated() {
-			g.P(deprecationComment)
-		}
-		g.P(method.Comments.Leading,
-			clientSignature(g, method))
-
-		// 不是流方法
-		if !method.Desc.IsStreamingClient() && !method.Desc.IsStreamingServer() {
-			// 异步方法
-			g.P(method.Comments.Leading,
-				clientASyncSignature(g, method))
-		}
-	}
-
-	g.P("}")
-	g.P()
-
-	// Client structure.
-	g.P("type ", unexport(clientName), " struct {")
-	g.P("cc ", kitePackage.Ident("IClient"))
-	g.P("}")
-	g.P()
-
 	// 全局
-	g.P("var ", export(service.GoName), " = &", unexport(service.GoName), "{}")
+	g.P("var ", export(service.GoName), " = &", unExport(service.GoName), "{}")
 	g.P()
 
-	g.P("type ", unexport(service.GoName), " struct {")
+	g.P("type ", unExport(service.GoName), " struct {")
 	g.P("}")
 	g.P()
 
@@ -104,15 +59,6 @@ func genService(gen *protogen.Plugin, file *protogen.File, g *protogen.Generated
 	if service.Desc.Options().(*descriptorpb.ServiceOptions).GetDeprecated() {
 		g.P(deprecationComment)
 	}
-	g.P("func New", clientName, " (cc ", kitePackage.Ident("IClient"), ") ", clientName, " {")
-	g.P("return &", unexport(clientName), "{cc}")
-	g.P("}")
-	g.P()
-
-	g.P("func Get", clientName, " (serviceInfo ", kite.Ident("ServiceInfo"), ") ", clientName, " {")
-	g.P("return &", unexport(clientName), "{", kiteClient.Ident("GetClient(serviceInfo)"), "}")
-	g.P("}")
-	g.P()
 
 	var methodIndex, streamIndex int
 	// Client method implementations.
@@ -129,7 +75,7 @@ func genService(gen *protogen.Plugin, file *protogen.File, g *protogen.Generated
 	}
 
 	// Server interface.
-	serverType := service.GoName + "Server"
+	serverType := export(service.GoName) + "Server"
 	g.P("// ", serverType, " is the server API for ", service.GoName, " service.")
 	if service.Desc.Options().(*descriptorpb.ServiceOptions).GetDeprecated() {
 		g.P("//")
@@ -152,203 +98,76 @@ func genService(gen *protogen.Plugin, file *protogen.File, g *protogen.Generated
 	if service.Desc.Options().(*descriptorpb.ServiceOptions).GetDeprecated() {
 		g.P(deprecationComment)
 	}
-	serviceDescVar := service.GoName + "_serviceDesc"
-	g.P("func Register", service.GoName, "Server(s *", kitePackage.Ident("Server"), ", srv ", serverType, ") {")
-	g.P("s.RegisterService(&", serviceDescVar, `, srv)`)
+
+	g.P("type ", export(service.GoName), "Service struct {")
+	g.P("handle ", export(service.GoName), "Server")
 	g.P("}")
 	g.P()
 
-	g.P("func Reg", service.GoName, "Server(srv ", serverType, ") ", kitePackage.Ident("Stub"), " {")
-	g.P("return ", kitePackage.Ident("Stub"), "{")
-	g.P("SD: &", serviceDescVar, ",")
-	g.P("SS: srv}")
+	fileName := path.Base(file.GeneratedFilenamePrefix)
+	g.P("func Reg", export(service.GoName), "Server(handle ", serverType, ") {")
+	g.P(g.QualifiedGoIdent(PB.Ident("ServiceDispatchObject.AddService")), `("`, fileName, `", "`, service.Desc.FullName(), `", &`, export(service.GoName), "Service{handle: handle})")
 	g.P("}")
 	g.P()
 
 	// Server handler implementations.
-	var handlerNames []string
-	for _, method := range service.Methods {
-		hname := genServerMethod(gen, file, g, method)
-		handlerNames = append(handlerNames, hname)
-	}
 
-	// Service descriptor.
-	g.P("var ", serviceDescVar, " = ", kitePackage.Ident("ServiceDesc"), " {")
-	g.P("ServiceName: ", strconv.Quote(string(service.Desc.FullName())), ",")
-	g.P("HandlerType: (*", serverType, ")(nil),")
-	g.P("Methods: []", kitePackage.Ident("MethodDesc"), "{")
-	for i, method := range service.Methods {
-		if method.Desc.IsStreamingClient() || method.Desc.IsStreamingServer() {
-			continue
-		}
-		g.P("{")
-		g.P("MethodName: ", strconv.Quote(string(method.Desc.Name())), ",")
-		g.P("Handler: ", handlerNames[i], ",")
-		g.P("},")
+	// generate DO
+	g.P("func (s *", export(service.GoName), "Service) Do(function string, reqPBData []byte) (resPBData []byte, err error) {")
+	g.P("switch function {")
+	for _, method := range service.Methods {
+		g.P(`case "`, method.GoName, `":`)
+		g.P("return s.", method.GoName, `(function, reqPBData)`)
 	}
-	g.P("},")
-	g.P("Metadata: \"", file.Desc.Path(), "\",")
+	g.P("default:")
+	g.P(`err = `, g.QualifiedGoIdent(errors.Ident(`New("function is not found")`)))
+	g.P("}")
+	g.P("return")
 	g.P("}")
 	g.P()
+
+	// generate function
+	for _, method := range service.Methods {
+		genServerMethod(gen, file, g, method)
+	}
 }
 
 func clientSignature(g *protogen.GeneratedFile, method *protogen.Method) string {
 	s := method.GoName + "("
+	s += "destination " + g.QualifiedGoIdent(kite.Ident("Destination"))
 	if !method.Desc.IsStreamingClient() {
-		s += "in *" + g.QualifiedGoIdent(method.Input.GoIdent)
+		s += ", request *" + g.QualifiedGoIdent(method.Input.GoIdent)
 	}
-	s += ", opts ..." + g.QualifiedGoIdent(kitePackage.Ident("CallOption")) + ") ("
+	s += ", opts ..." + g.QualifiedGoIdent(kite.Ident("Option")) + ") ("
 	if !method.Desc.IsStreamingClient() && !method.Desc.IsStreamingServer() {
-		s += "*" + g.QualifiedGoIdent(method.Output.GoIdent)
+		s += "response *" + g.QualifiedGoIdent(method.Output.GoIdent)
 	} else {
 		s += method.Parent.GoName + "_" + method.GoName + "Client"
 	}
-	s += ", error)"
+	s += ", err error) "
 	return s
 }
-
-// clientASyncSignature 异步方法
-func clientASyncSignature(g *protogen.GeneratedFile, method *protogen.Method) string {
-	s := kiteAsync + method.GoName + "("
-	if !method.Desc.IsStreamingClient() {
-		s += "in *" + g.QualifiedGoIdent(method.Input.GoIdent)
-	}
-	s += ", opts ..." + g.QualifiedGoIdent(kitePackage.Ident("CallOption")) + ") "
-	s += "*" + g.QualifiedGoIdent(kitePackage.Ident("Call"))
-	return s
-}
-
-// 全局----
-func signature(g *protogen.GeneratedFile, method *protogen.Method) string {
-	s := method.GoName + "("
-	s += "serviceInfo " + g.QualifiedGoIdent(kite.Ident("ServiceInfo")) + ", "
-	if !method.Desc.IsStreamingClient() {
-		s += "in *" + g.QualifiedGoIdent(method.Input.GoIdent)
-	}
-	s += ", opts ..." + g.QualifiedGoIdent(kitePackage.Ident("CallOption")) + ") ("
-	if !method.Desc.IsStreamingClient() && !method.Desc.IsStreamingServer() {
-		s += "*" + g.QualifiedGoIdent(method.Output.GoIdent)
-	} else {
-		s += method.Parent.GoName + "_" + method.GoName + "Client"
-	}
-	s += ", error)"
-	return s
-}
-
-// aSyncSignature 异步方法
-func aSyncSignature(g *protogen.GeneratedFile, method *protogen.Method) string {
-	s := kiteAsync + method.GoName + "("
-	s += "serviceInfo " + g.QualifiedGoIdent(kite.Ident("ServiceInfo")) + ", "
-	if !method.Desc.IsStreamingClient() {
-		s += "in *" + g.QualifiedGoIdent(method.Input.GoIdent)
-	}
-	s += ", opts ..." + g.QualifiedGoIdent(kitePackage.Ident("CallOption")) + ") "
-	s += "*" + g.QualifiedGoIdent(kitePackage.Ident("Call"))
-	return s
-}
-
-// 全局-----
 
 func genClientMethod(gen *protogen.Plugin, file *protogen.File, g *protogen.GeneratedFile, method *protogen.Method, index int) {
 	service := method.Parent
 	// 获取文件名
 	fileName := path.Base(file.GeneratedFilenamePrefix)
-	sname := fmt.Sprintf("%s/%s/%s", fileName, service.Desc.FullName(), method.Desc.Name())
-
 	if method.Desc.Options().(*descriptorpb.MethodOptions).GetDeprecated() {
 		g.P(deprecationComment)
 	}
-	g.P("func (c *", unexport(service.GoName), "Client) ", clientSignature(g, method), "{")
+	g.P("func (c *", unExport(service.GoName), ") ", clientSignature(g, method), "{")
 	if !method.Desc.IsStreamingServer() && !method.Desc.IsStreamingClient() {
-		g.P("out := new(", method.Output.GoIdent, ")")
-		g.P(`err := c.cc.Invoke("`, sname, `", in, out, opts...)`)
+		g.P("reqPBData, err := ", g.QualifiedGoIdent(PB.Ident("Marshal(request)")))
+		g.P("if err != nil { return nil, ", g.QualifiedGoIdent(errors.Ident(`New("request marshal err")}`)))
+
+		g.P(`resPBData, err := `, g.QualifiedGoIdent(kite.Ident("Invoke")), `(destination, "`, fileName, `", "`, service.Desc.FullName(), `", "`, method.Desc.Name(), `", reqPBData, opts...)`)
 		g.P("if err != nil { return nil, err }")
-		g.P("return out, nil")
-		g.P("}")
-		g.P()
-
-		// 异步方法
-		g.P("func (c *", unexport(service.GoName), "Client) ", clientASyncSignature(g, method), "{")
-		g.P("out := new(", method.Output.GoIdent, ")")
-		g.P(`return c.cc.AsyncInvoke("`, sname, `", in, out, opts...)`)
-		g.P("}")
-		g.P()
-
-		// 全局
-		g.P("func (c *", unexport(service.GoName), ") ", signature(g, method), "{")
-		g.P("cli := &", unexport(service.GoName), "Client{", g.QualifiedGoIdent(kiteClient.Ident("GetClient")), "(serviceInfo)}")
-		g.P("return cli.", method.GoName, "(in, opts...)")
-		g.P("}")
-		g.P()
-
-		// 全局-异步方法
-		g.P("func (c *", unexport(service.GoName), ") ", aSyncSignature(g, method), "{")
-		g.P("cli := &", unexport(service.GoName), "Client{", g.QualifiedGoIdent(kiteClient.Ident("GetClient")), "(serviceInfo)}")
-		g.P("return cli.", kiteAsync, method.GoName, "(in, opts...)")
-		g.P("}")
+		g.P("response = new(", method.Output.GoIdent, ")")
+		g.P("err = ", g.QualifiedGoIdent(PB.Ident("Unmarshal(resPBData, response)")))
+		g.P("return")
 		g.P()
 
 		return
-	}
-	streamType := unexport(service.GoName) + method.GoName + "Client"
-	serviceDescVar := service.GoName + "_serviceDesc"
-	g.P("stream, err := c.cc.NewStream(&", serviceDescVar, ".Streams[", index, `], "`, sname, `", opts...)`)
-	g.P("if err != nil { return nil, err }")
-	g.P("x := &", streamType, "{stream}")
-	if !method.Desc.IsStreamingClient() {
-		g.P("if err := x.ClientStream.SendMsg(in); err != nil { return nil, err }")
-		g.P("if err := x.ClientStream.CloseSend(); err != nil { return nil, err }")
-	}
-	g.P("return x, nil")
-	g.P("}")
-	g.P()
-
-	genSend := method.Desc.IsStreamingClient()
-	genRecv := method.Desc.IsStreamingServer()
-	genCloseAndRecv := !method.Desc.IsStreamingServer()
-
-	// Stream auxiliary types and methods.
-	g.P("type ", service.GoName, "_", method.GoName, "Client interface {")
-	if genSend {
-		g.P("Send(*", method.Input.GoIdent, ") error")
-	}
-	if genRecv {
-		g.P("Recv() (*", method.Output.GoIdent, ", error)")
-	}
-	if genCloseAndRecv {
-		g.P("CloseAndRecv() (*", method.Output.GoIdent, ", error)")
-	}
-	g.P(kitePackage.Ident("ClientStream"))
-	g.P("}")
-	g.P()
-
-	g.P("type ", streamType, " struct {")
-	g.P(kitePackage.Ident("ClientStream"))
-	g.P("}")
-	g.P()
-
-	if genSend {
-		g.P("func (x *", streamType, ") Send(m *", method.Input.GoIdent, ") error {")
-		g.P("return x.ClientStream.SendMsg(m)")
-		g.P("}")
-		g.P()
-	}
-	if genRecv {
-		g.P("func (x *", streamType, ") Recv() (*", method.Output.GoIdent, ", error) {")
-		g.P("m := new(", method.Output.GoIdent, ")")
-		g.P("if err := x.ClientStream.RecvMsg(m); err != nil { return nil, err }")
-		g.P("return m, nil")
-		g.P("}")
-		g.P()
-	}
-	if genCloseAndRecv {
-		g.P("func (x *", streamType, ") CloseAndRecv() (*", method.Output.GoIdent, ", error) {")
-		g.P("if err := x.ClientStream.CloseSend(); err != nil { return nil, err }")
-		g.P("m := new(", method.Output.GoIdent, ")")
-		g.P("if err := x.ClientStream.RecvMsg(m); err != nil { return nil, err }")
-		g.P("return m, nil")
-		g.P("}")
-		g.P()
 	}
 }
 
@@ -367,81 +186,26 @@ func serverSignature(g *protogen.GeneratedFile, method *protogen.Method) string 
 	return method.GoName + "(" + strings.Join(reqArgs, ", ") + ") " + ret
 }
 
-func genServerMethod(gen *protogen.Plugin, file *protogen.File, g *protogen.GeneratedFile, method *protogen.Method) string {
+func genServerMethod(gen *protogen.Plugin, file *protogen.File, g *protogen.GeneratedFile, method *protogen.Method) {
 	service := method.Parent
-	hname := fmt.Sprintf("_%s_%s_Handler", service.GoName, method.GoName)
 
 	if !method.Desc.IsStreamingClient() && !method.Desc.IsStreamingServer() {
-		g.P("func ", hname, "(srv interface{}, dec func(interface{}) error) (interface{}, error) {")
-		g.P("in := new(", method.Input.GoIdent, ")")
-		g.P("if err := dec(in); err != nil { return nil, err }")
-		g.P("return srv.(", service.GoName, "Server).", method.GoName, "(in)")
+		g.P("func (s *", export(service.GoName), "Service) ", method.GoName, "Add(function string, reqPBData []byte) (resPBData []byte, err error) {")
+		g.P("req := new(", method.Input.GoIdent, ")")
+		g.P(g.QualifiedGoIdent(PB.Ident("Unmarshal(reqPBData, req)")))
+		g.P("res := new(", method.Output.GoIdent, ")")
+		g.P("res, err = s.handle.", method.GoName, "(req)")
+		g.P("if err == nil { resPBData, err = pb.Marshal(res) }")
+		g.P("return")
 		g.P("}")
-		g.P()
-		return hname
+		g.P("")
+		return
 	}
-	streamType := unexport(service.GoName) + method.GoName + "Server"
-	g.P("func ", hname, "(srv interface{}, stream ", kitePackage.Ident("ServerStream"), ") error {")
-	if !method.Desc.IsStreamingClient() {
-		g.P("m := new(", method.Input.GoIdent, ")")
-		g.P("if err := stream.RecvMsg(m); err != nil { return err }")
-		g.P("return srv.(", service.GoName, "Server).", method.GoName, "(m, &", streamType, "{stream})")
-	} else {
-		g.P("return srv.(", service.GoName, "Server).", method.GoName, "(&", streamType, "{stream})")
-	}
-	g.P("}")
-	g.P()
-
-	genSend := method.Desc.IsStreamingServer()
-	genSendAndClose := !method.Desc.IsStreamingServer()
-	genRecv := method.Desc.IsStreamingClient()
-
-	// Stream auxiliary types and methods.
-	g.P("type ", service.GoName, "_", method.GoName, "Server interface {")
-	if genSend {
-		g.P("Send(*", method.Output.GoIdent, ") error")
-	}
-	if genSendAndClose {
-		g.P("SendAndClose(*", method.Output.GoIdent, ") error")
-	}
-	if genRecv {
-		g.P("Recv() (*", method.Input.GoIdent, ", error)")
-	}
-	g.P(kitePackage.Ident("ServerStream"))
-	g.P("}")
-	g.P()
-
-	g.P("type ", streamType, " struct {")
-	g.P(kitePackage.Ident("ServerStream"))
-	g.P("}")
-	g.P()
-
-	if genSend {
-		g.P("func (x *", streamType, ") Send(m *", method.Output.GoIdent, ") error {")
-		g.P("return x.ServerStream.SendMsg(m)")
-		g.P("}")
-		g.P()
-	}
-	if genSendAndClose {
-		g.P("func (x *", streamType, ") SendAndClose(m *", method.Output.GoIdent, ") error {")
-		g.P("return x.ServerStream.SendMsg(m)")
-		g.P("}")
-		g.P()
-	}
-	if genRecv {
-		g.P("func (x *", streamType, ") Recv() (*", method.Input.GoIdent, ", error) {")
-		g.P("m := new(", method.Input.GoIdent, ")")
-		g.P("if err := x.ServerStream.RecvMsg(m); err != nil { return nil, err }")
-		g.P("return m, nil")
-		g.P("}")
-		g.P()
-	}
-
-	return hname
+	return
 }
 
 const deprecationComment = "// Deprecated: Do not use."
 
-func unexport(s string) string { return strings.ToLower(s[:1]) + s[1:] }
+func unExport(s string) string { return strings.ToLower(s[:1]) + s[1:] }
 
 func export(s string) string { return strings.ToUpper(s[:1]) + s[1:] }
